3d108a54d6532dfa23786419893005e73ae76342
bw0711 <bw0711@gmail.com>
2023-12-21 11:37:23 +0200

    homework for pw

---
+++ tests/initialState.tests.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Check initial state', async ({ page }) => {
+    await page.goto('/');
+
+    const expectedTexts = [
+        "Elements",
+        "Forms",
+        "Alerts, Frame & Windows",
+        "Widgets",
+        "Interactions",
+        "Book Store Application"
+    ];
+
+    for (const text of expectedTexts) {
+        await expect(page.locator(`text=${text}`)).toBeVisible();
+    }
+});
