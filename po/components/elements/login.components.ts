c9137c389d211704761a584ad26dec3434b88439
bw0711 <bw0711@gmail.com>
2023-12-27 15:55:06 +0200

    tests for login page + new workflow

---
+++ po/components/elements/login.components.ts
@@ -0,0 +1,29 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginComponent {
+  constructor(private page: Page) {}
+
+  public get component(): Locator {
+    return this.page.locator('#item-0', { has: this.page.getByText('Login') });
+  }
+
+  public get userNameField(): Locator {
+    return this.page.locator('#userName');
+  }
+
+  public get passwordField(): Locator {
+    return this.page.locator('#password');
+  }
+
+  public get loginButton(): Locator {
+    return this.page.locator('#login');
+  }
+
+  public get newUserButton(): Locator {
+    return this.page.locator('#newUser');
+  }
+
+  async navigateToComponent() {
+    await this.component.click();
+  }
+}
--- po/pages/bookstore.page.ts
+++ po/pages/bookstore.page.ts
@@ -1,16 +1,39 @@
 import { Locator, Page } from '@playwright/test';
 import { BasePage } from './base.page';
+import { LoginComponent } from 'po/components/elements/login.components';
 
 export class BookStorePage extends BasePage {
   readonly category: Locator;
+  readonly loginPage: LoginComponent;
 
   constructor(page: Page) {
     super(page);
     this.category = this.bookStoreLocator;
+    this.loginPage = new LoginComponent(page);
+  }
+
+  // public get loggedUserNameValue(): Promise<string> {
+  //   return this.page.locator('#userName-value').textContent() || ''; 
+  // }
+
+  async loggedUserNameValue(): Promise<string> {
+    const textContent = await this.page.locator('#userName-value').textContent();
+    return textContent || '';
   }
 
   async navigateToCategory() {
     await this.navigateToHomePage();
     await this.category.click();
+  }
+
+  async navigateToLoginPage() {
+    await this.navigateToCategory();
+    await this.loginPage.navigateToComponent();
+  }
+
+  async loginUser() {
+    await this.loginPage.userNameField.fill(process.env.USERNAME || '');
+    await this.loginPage.passwordField.fill(process.env.PASSWORD || '');
+    await this.loginPage.loginButton.click();
   }
 }
